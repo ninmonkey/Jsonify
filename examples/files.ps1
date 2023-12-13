cff3247873a0a2e08257ea186ab62a691ba6732d
Jake Bolton <ninmonkeys@gmail.com>
2023-12-13 17:45:10 -0600

    add Example cases before the module

--- examples/files.ps1
+++ examples/files.ps1
@@ -1,15 +1,27 @@
-﻿impo Jsonify -force -PassThru
+﻿# impo Jsonify -force -PassThru
+Import-Module (Join-Path $PSScriptRoot '../Jsonify/Jsonify.psm1')  -force -PassThru
+    | Render.ModuleName
+Set-Alias 'Json.Original' -Value Microsoft.PowerShell.Utility\ConvertTo-Json
 
-# [Text.StringBuilder]$Sb = ''
-# return
+
 $file = Get-item $PSCommandPath
 $folder = get-ITem $PSScriptRoot
 
-$file | ConvertTo-Json -compress -depth 3
-$folder| ConvertTo-Json -compress -depth 3
+$cache = @{}
+$cache.File_D3   ??= $file | ConvertTo-Json -compress -depth 3
+$cache.File_D1   ??= $file | ConvertTo-Json -compress -depth 1
+$cache.Folder_D3 ??= $Folder | ConvertTo-Json -depth 3
+$cache.Folder_D1 ??= $Folder | ConvertTo-Json -depth 1
+$cache.Folder_D0 ??= $Folder | ConvertTo-Json -depth 0
 
 h1 'for files, even depth 1/0 is huge'
-$file|ConvertTo-Json -Depth 1 -Compress
 
-
-'and now'
+@(foreach($Item in $cache.GetEnumerator()) {
+    [pscustomobject]@{
+        Name   = ($Item.Key -split '_')[0]
+        Depth  = ($Item.Key -split '_')[-1] -replace '\D', ''
+        JsonLength = $Item.Value.Length
+    }
+})
+| Sort-Object Length -Descending
+| Ft -auto
