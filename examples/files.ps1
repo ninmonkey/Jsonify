32568ce74639ee494cffbe7c28330544ff1cc7b2
Jake Bolton <ninmonkeys@gmail.com>
2023-12-14 11:10:37 -0600

    add comparison

--- examples/files.ps1
+++ examples/files.ps1
@@ -3,18 +3,53 @@ Import-Module (Join-Path $PSScriptRoot '../Jsonify/Jsonify.psm1')  -force -PassT
     | Render.ModuleName
 Set-Alias 'Json.Original' -Value Microsoft.PowerShell.Utility\ConvertTo-Json
 
+$file = Get-Item $PSCommandPath
+$folder = Get-Item $PSScriptRoot
+$sharedJson = @{
+    Compress = $True
+    WarningAction = 'ignore'
+}
 
-$file = Get-item $PSCommandPath
-$folder = get-ITem $PSScriptRoot
+<#
+output:
+
+    Name   Depth JsonLength
+    ----   ----- ----------
+    File   0           1618
+    File   1           2970
+    File   3         373007
+    Folder 0           1014
+    Folder 1           2584
+    Folder 3         373325
+
+
+    Name   Template JsonLength
+    ----   -------- ----------
+    File   Basic           173
+    File   Minify           17
+    Folder Basic           537
+    Folder Minify          144
+#>
 
 $cache = @{}
-$cache.File_D3   ??= $file | ConvertTo-Json -compress -depth 3
-$cache.File_D1   ??= $file | ConvertTo-Json -compress -depth 1
-$cache.Folder_D3 ??= $Folder | ConvertTo-Json -depth 3
-$cache.Folder_D1 ??= $Folder | ConvertTo-Json -depth 1
-$cache.Folder_D0 ??= $Folder | ConvertTo-Json -depth 0
+$cache.File_D3   ??= $file | Json.Original @sharedJson -depth 3
+$cache.File_D1   ??= $file | Json.Original @sharedJson -depth 1
+$cache.File_D0   ??= $file | Json.Original @sharedJson -depth 0
+$cache.Folder_D3 ??= $Folder | Json.Original @sharedJson -depth 3
+$cache.Folder_D1 ??= $Folder | Json.Original @sharedJson -depth 1
+$cache.Folder_D0 ??= $Folder | Json.Original @sharedJson -depth 0
 
-h1 'for files, even depth 1/0 is huge'
+$results_jsonify = [ordered]@{}
+$results_jsonify.Folder_Basic =
+    CoerceFrom.FileSystemInfo $folder  -TemplateName Basic | Json -Compress
+$results_jsonify.Folder_Minify =
+    CoerceFrom.FileSystemInfo $folder  -TemplateName Minify | Json -Compress
+$results_jsonify.File_Basic =
+    CoerceFrom.FileSystemInfo $File  -TemplateName Basic | Json -Compress
+$results_jsonify.File_Minify =
+    CoerceFrom.FileSystemInfo $File  -TemplateName Minify | Json -Compress
+
+'for files, even -Depth 1/0 is huge' | write-warning
 
 @(foreach($Item in $cache.GetEnumerator()) {
     [pscustomobject]@{
@@ -23,5 +58,15 @@ h1 'for files, even depth 1/0 is huge'
         JsonLength = $Item.Value.Length
     }
 })
-| Sort-Object Length -Descending
+| Sort-Object Name, Depth, JsonLength
+| Ft -auto
+@(foreach($Item in $results_jsonify.GetEnumerator()) {
+    [pscustomobject]@{
+        Name   = ($Item.Key -split '_')[0]
+        Template  = ($Item.Key -split '_')[-1]
+        JsonLength = $Item.Value.Length
+    }
+})
+# | Sort-Object Length
+| Sort-Object Name, Template, JsonLength
 | Ft -auto
